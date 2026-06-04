5b0da9c887a350afedfe2ef605868715a7d32efd
Jose-RCastle <143913314+Jose-RCastle@users.noreply.github.com>
2026-06-03 19:16:06 -0600

    Add DS Lab tab

--- FlightBookingApp/src/navigation/TabsNavigator.tsx
+++ FlightBookingApp/src/navigation/TabsNavigator.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import MyReservationsScreen from "../screens/MyReservationsScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import DataStructuresDemoScreen from "../screens/DataStructuresDemoScreen";
 import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
@@ -21,6 +22,8 @@ export default function TabsNavigator() {
             iconName = "airplane";
           } else if (route.name === "Perfil") {
             iconName = "person";
+          } else if (route.name === "DS Lab") {
+            iconName = "flask";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -32,6 +35,7 @@ export default function TabsNavigator() {
     >
       <Tab.Screen name="Inicio" component={HomeScreen} />
       <Tab.Screen name="Reservas" component={MyReservationsScreen} />
+      <Tab.Screen name="DS Lab" component={DataStructuresDemoScreen} />
       <Tab.Screen name="Perfil" component={ProfileScreen} />
     </Tab.Navigator>
   );
